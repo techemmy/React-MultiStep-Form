f9448d634b0a0123b281cd1abf036788a18c3518
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-24 00:00:04 +0100

    fix: make step 4 active on thank you page to match design

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,8 +13,9 @@ export default function Sidebar({ step: currentStep, handleStepChange }) {
         className="counter__list gap-x-4 mt-3 ssmax:flex ssmax:flex-row ssmax:justify-center ssmax:items-start ssmax:h-full">
         {Object.keys(steps).map(step => {
           const activeState = parseInt(step) === currentStep ? 'active' : ''
+          const activeThankYouPage = !Object.keys(steps).includes(currentStep.toString()) && parseInt(step) === 4 ? 'active' : ''
           return (
-            <li key={`step-${steps[step]}`} onClick={() => handleStepChange(parseInt(step))} className={`${activeState} counter flex items-center mb-5 ssmin:before:mr-5`} >
+            <li key={`step-${steps[step]}`} onClick={() => handleStepChange(parseInt(step))} className={`${activeState} counter flex items-center mb-5 ssmin:before:mr-5 ${activeThankYouPage}`} >
               <p className="ssmax:hidden flex flex-col justify-center">
                 <span className="text-coolGray">Step {step}</span>
                 <span className="text-white font-medium">{steps[step]}</span>
