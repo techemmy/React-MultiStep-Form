6dbcc613d380b241aca78a0d150dbaf39742c9eb
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-26 17:05:12 +0100

    fix: persist switch state in Form Step 2

--- src/components/FormStep2.jsx
+++ src/components/FormStep2.jsx
@@ -1,5 +1,7 @@
+import { PLANS } from "../constants";
 import PlanCard from "./PlanCard";
 export default function FormStep2({ plan, planOptions, handlePlanChange, selectedPlanOption, handlePlanOptionChange }) {
+  const plans = Object.keys(PLANS);
   return (
     <form
       className="pb-6 ssmax:mx-5 ssmax:px-6 ssmax:pt-6 ssmax:rounded-lg bg-white ssmax:relative ssmax:top-[-103px]">
@@ -24,7 +26,10 @@ export default function FormStep2({ plan, planOptions, handlePlanChange, selecte
       <section className="font-medium bg-alabaster py-2 gap-x-4 mt-10 flex flex-row justify-center items-center">
         <p className={plan === 'monthly' ? "" : "text-coolGray"}>Monthly</p>
         <label className="toggle__switch">
-          <input type="checkbox" onChange={(e) => handlePlanChange(e.target.checked)} defaultChecked />
+          <input
+            type="checkbox"
+            onChange={(e) => handlePlanChange(e.target.checked)}
+            checked={plan === plans[0] ? true : false} />
           <span className="slider round"></span>
         </label>
         <p className={plan === 'yearly' ? "" : "text-coolGray"}>Yearly</p>
